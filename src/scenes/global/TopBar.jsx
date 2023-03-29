31b34f324abe46e30155462d4fc3a2deb42957b9
FayozbekOlimov <fayozbek2021dev@gmail.com>
2023-03-29 17:26:47 +0500

    mui customization

---
+++ src/scenes/global/TopBar.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { Box, IconButton, useTheme } from "@mui/material";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+const TopBar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return <Box display="flex" justifyContent="space-between" p={2}>
+    {/* SEARCH BAR */}
+    <IconButton></IconButton>
+  </Box>;
+};
+
+export default TopBar;
